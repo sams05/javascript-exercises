3d3fabdf013f42e56e0b3766b0a0115744ec6cb2
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-28 23:36:31 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const char = str.split(""); // Split the string into an array individual characters
+    const charReversed = [];
 
+    for(let i = str.length - 1; i >= 0; i--) {
+        charReversed.push(char[i]);
+    }
+
+    return charReversed.join("");
+
+    /*
+        Solution with built-in array function:
+        return char.reversed().join("");
+    */
 };
 
 // Do not edit below this line
