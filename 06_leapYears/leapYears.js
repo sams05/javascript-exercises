210a1ccc240494e4c6d14a0de9dc8313bf131195
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-29 01:23:02 -0400

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleBy4 = year % 4 === 0;
+    const isDivisibleBy100 = year % 100 === 0;
+    const isDivisibleBy400 = year % 400 === 0;
+    let isLeapYear;
 
+    if(isDivisibleBy4) {
+        isLeapYear = true;
+        if(isDivisibleBy100) {
+            isLeapYear = false;
+            if(isDivisibleBy400) {
+                isLeapYear = true;
+            }
+        }
+    } else {
+        isLeapYear = false;
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
