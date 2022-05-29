51de86f287cfc41fc6a0670bb705eed77000e251
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-29 00:58:40 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    // Make num1 the smaller of the two numbers
+    if(num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    sum = 0;
+    for(let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
