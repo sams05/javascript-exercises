5f47117aed5b311eb58fef28fa6c92dcf3319aa3
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-31 13:30:31 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n; // Support strings as argument
+    if(n < 1) return 'OOPS';
 
+    if(n === 1 || n === 2) {
+        return 1;
+    }
+    let [prevPrev, prev] = [1, 1];
+    let curTerm;
+    for(let i = 3; i <= n; i++) {
+        curTerm = prevPrev + prev;
+
+        prevPrev = prev;
+        prev = curTerm;
+    }
+    return curTerm;
 };
 
 // Do not edit below this line
