5e7f6a73033c6f036ee0d7aedb47fb80e0e76742
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-31 12:55:57 -0400

    Complete exercise 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let runningFactorial = 1;
+  for(let i = 2; i <= num; i++) {
+    runningFactorial *= i;
+  }
+  return runningFactorial;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    // Make into an array and filter out non-alphabetical characters
+    const lettersArr = str.split('').filter(char => char.match(/[a-z]/));
 
+    const letters = lettersArr.join('');
+    const lettersReversed = lettersArr.reverse().join('');
+    return letters === lettersReversed;
 };
 
 // Do not edit below this line
