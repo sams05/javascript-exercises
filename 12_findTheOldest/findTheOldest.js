7cf0a994f7c0bb814a485c5fc56742c4ddd9d50b
Samuel Shen <sshen05@terpmail.umd.edu>
2022-05-31 14:07:13 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestAge = people.reduce((oldestAge, person) => {
+        // Using || operator
+        // const endYear = person.yearOfDeath || (new Date()).getFullYear();
+        const endYear = ('yearOfDeath' in person) ? person.yearOfDeath : (new Date()).getFullYear();
+        age = endYear - person.yearOfBirth;
+        if(age > oldestAge) {
+            oldestAge = age;
+        }
+        return oldestAge;
+    }, 0);
 
+    const oldestAgeIdx = people.findIndex(person => {
+        const endYear = ('yearOfDeath' in person) ? person.yearOfDeath : (new Date()).getFullYear();
+        return endYear - person.yearOfBirth === oldestAge
+    });
+
+    return people[oldestAgeIdx];
 };
 
 // Do not edit below this line
